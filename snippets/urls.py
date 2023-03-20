9b7badc1c5058d17e39e5dc5fce79b953097677d
alita <alita.avs@gmail.com>
2023-03-20 21:22:57 +0200

    all

---
+++ snippets/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from snippets import views
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename="snippet")
+router.register(r'users', views.UserViewSet, basename="user")
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
+# from django.urls import path, include
+# from rest_framework.urlpatterns import format_suffix_patterns
+# from snippets import views
+#
+# urlpatterns = [
+#     path('', views.api_root),
+#
+#     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+#     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+#
+#     path('users/', views.UserList.as_view(), name='user-list'),
+#     path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+#     path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+# ]
+#
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
+]
+#
+# urlpatterns = format_suffix_patterns(urlpatterns)
